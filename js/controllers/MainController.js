1fe2e9c1aba11a80f83252320f87c03d7bce3a8a
Lamar Russ <L627B@ErieInsurance.com>
2019-07-29 13:34:58 -0400

    take II

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,26 +1,5 @@
 app.controller('MainController', ['$scope', 'submissions', function($scope, submissions) {
-    submissions.success(function (data) {
-        $scope.actualListing = data;
+    submissions.getData().then(function (response) {
+        $scope.actualListing = response;
     });
-
-  $scope.listing = [{
-      title: "Born To Be Yours",
-      title_img: "img/BornToBeYours.png",
-      genre: "Pop",
-      level: 10,
-      length: 3.5,
-      pack: "Cringle Haven",
-      details: "Easy song, nothing to worry about here",
-      artist: "Imagine Dragons"
-    },
-    {
-      title: "Bubble Pop",
-      title_img: "img/BubblePop.png",
-      genre: "K-Pop",
-      level: 10,
-      length: 2.4,
-      pack: "AJAX's Magical Project Adventure",
-      details: "Just a test here boys",
-      artist: "No Idea"
-    }]
 }]);
--- js/services/submissions.js
+++ js/services/submissions.js
@@ -1,10 +1,18 @@
 app.factory('submissions', ['$http', function ($http) {
-    return
-    $http.get('https://crashcringle12.github.io/js/services/submissions.json')
-        .success(function (data) {
-            return data;
-        })
-        .error(function (err) {
-            return err;
-        });
+    var all, odds = [];
+    var getData = function () {
+        return $http.get("https://crashcringle12.github.io/js/services/submissions.json")
+            .then(function (response) {
+                all = response.records;
+                angular.forEach(all, function (c, i) {
+                    if (i % 2 == 1) {
+                        odds.push(c);
+                    }
+                });
+                return odds
+            });
+    }
+    return {
+        getData: getData
+    };
 }]);
