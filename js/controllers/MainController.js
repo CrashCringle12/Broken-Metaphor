cb73c2af2a6c8526b5412c21250d34b651c3744b
Lamar Russ <L627B@ErieInsurance.com>
2019-08-01 12:21:51 -0400

    Initial Clicking groundworks

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,5 +1,17 @@
 app.controller('MainController', ['$scope', 'submissions', function($scope, submissions) {
     submissions.getData().then(function (response) {
         $scope.listing = response;
+        $scope.color = 'Blue';
+        $scope.num = 0;
+        $scope.myFunction = function() {
+            $scope.num++;
+            if ($scope.num > 2) {
+                $scope.num = 0;
+                $scope.color = 'Blue';
+            }
+            else {
+                $scope.color = $scope.num == 1 ? 'Red' : 'Purple';
+            }
+        }
     });
 }]);
